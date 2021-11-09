52ff2ccb5b84141308f21c4ee4dd2840ffb057bb
Andrea <andr3z45@gmail.com>
2021-11-09 16:06:19 +0100

    inizio ciclo

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 for (let i = 1; i <= 100; i++){
     console.log(i);
+    if(i % 3 == 0){
+        console.log(i + ' multiplo di 3');
+    }
+    if(i % 5 == 0){
+        console.log(i+ ' multiplo di 5')
+    }
+    
 }
