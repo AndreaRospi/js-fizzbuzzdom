38a0b80de68fb902de5ce4ace188f8471c5a14e9
Andrea <andr3z45@gmail.com>
2021-11-09 17:47:18 +0100

    milestone1- milestone2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const box= document.getElementById("lista")
+const griglia= document.getElementById("griglia")
 let x= 0;
 
 for (let i = 1; i <= 100; i++){
@@ -11,7 +11,5 @@ for (let i = 1; i <= 100; i++){
     } else{
         x = i;
     }
-    if (isNaN(x)){
-        box.innerHTML
-    }
+    griglia.innerHTML += `<div class="box">${x}</div>`
 }
